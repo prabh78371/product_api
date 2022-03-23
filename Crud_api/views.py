325b34619c64c86607a8c9e7d119c6fd6c7a460f
prabhjot kaur <priya998815@gmail.com>
2022-03-23 17:43:14 +0530

    add

---
+++ Crud_api/views.py
@@ -0,0 +1,47 @@
+from functools import partial
+from urllib import response
+from django.shortcuts import render
+from itsdangerous import Serializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializer import productserializer
+from .models import Product
+
+# Create your views here.
+@api_view(['GET','POST','PUT','DELETE'])
+def product_api(request,pk=None):
+    if request.method ==  'GET':
+        id = pk
+        if id is not None:
+            prod = Product.objects.get(id=id)
+            serilizer = productserializer(prod)
+            return Response(serilizer.data)
+        prod = Product.objects.all()
+        serilizer = productserializer(prod,many=True)
+        return Response(serilizer.data)
+
+    if request.method == 'POST':
+        serilizer = productserializer(data=request.data)
+        if serilizer.is_valid():
+            serilizer.save()
+            return Response({'msg':'created'})
+        return Response(serilizer.errors)
+    
+    if request.method == 'PUT':
+        id = pk
+        prod = Product.objects.get(pk=id)
+        serilizer = productserializer(prod,data = request.data ,partial =True )
+        if serilizer.is_valid():
+            serilizer.save()
+            return Response({'msg':'updated'})
+        return Response(serilizer.errors)  
+
+    if request.method == 'DELETE':
+        id = pk
+        prod = Product.objects.get(pk=id)
+        prod.delete()
+        return Response({'msg':'deleted'})
+    
+
+
+
